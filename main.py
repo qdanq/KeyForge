7314ac4414f69c7d48050d97c8fefb47832320c2
qqbeach <danildzekelev@gmail.com>
2023-03-17 12:35:59 +0200

    Add password length choice

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import string, random
+import random
+import string
 
-rand = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(12)])
+
+class PasswordOptions:
+    def __init__(self, password_len):
+        self.password_len = password_len
+
+
+try:
+    get_password_len = int(input("Enter length of password(default - 16): "))
+except ValueError:
+    get_password_len = 16
+
+options = PasswordOptions(get_password_len)
+
+rand = ''.join(
+    [random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(options.password_range)])
 print(rand)
