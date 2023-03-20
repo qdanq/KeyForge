674b556109e8a108996f8e7df7199de32ec252a3
qqbeach <danildzekelev@gmail.com>
2023-03-20 21:32:00 +0200

    Add uppercase letters include option

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import string
 
 
 class PasswordOptions:
-    def __init__(self, password_len):
+    def __init__(self, password_len, uppercase_include):
         self.password_len = password_len
+        self.uppercase_include = uppercase_include
 
 
 try:
@@ -12,8 +13,13 @@ try:
 except ValueError:
     get_password_len = 16
 
-options = PasswordOptions(get_password_len)
+get_uppercase_include = input("Do you want to include uppercase letters?(default: yes, type '0' to not include)")
+if get_uppercase_include != "0":
+    get_uppercase_include = string.ascii_uppercase
+
+options = PasswordOptions(get_password_len, get_uppercase_include)
 
 rand = ''.join(
-    [random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(options.password_len)])
+    [random.choice(options.uppercase_include + string.ascii_lowercase + string.digits + string.punctuation) for n in
+     range(options.password_len)])
 print(rand)
