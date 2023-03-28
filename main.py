2f70eafcfd274158749dac77a607a645867a14a5
qqbeach <danildzekelev@gmail.com>
2023-03-28 22:22:39 +0300

    Change class generate_password to function Rename variable option to password

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import random
 import string
 
 
-class PasswordGenerator:
-    def __init__(self, password_len, uppercase_include, lowercase_include):
-        self.password_len = password_len
-        self.uppercase_include = uppercase_include
-        self.lowercase_include = lowercase_include
-
-    def __str__(self):
-        return ''.join(
-            [random.choice(self.uppercase_include + self.lowercase_include + string.digits + string.punctuation) for
-             n in
-             range(self.password_len)])
+def generate_password(password_len, uppercase_include=True, lowercase_include=True):
+    characters = ''
+    if uppercase_include != "n":
+        characters += string.ascii_uppercase
+    if lowercase_include != "n":
+        characters += string.ascii_lowercase
+    return ''.join(random.choice(characters) for _ in range(password_len))
 
 
 try:
@@ -20,14 +16,10 @@ try:
 except ValueError:
     get_password_len = 16
 
-get_uppercase_include = input("Do you want to include uppercase letters?(default: yes, type '0' to not include)")
-if get_uppercase_include != "0":
-    get_uppercase_include = string.ascii_uppercase
+get_uppercase_include = input("Do you want to include uppercase letters? (y/n): ").lower() == 'y'
 
-get_lowercase_include = input("Do you want to include lowercase letters?(default: yes, type '0' to not include")
-if get_lowercase_include != "0":
-    get_lowercase_include = string.ascii_lowercase
+get_lowercase_include = input(
+    "Do you want to include lowercase letters? (y/n): ").lower() == 'y'
 
-options = PasswordGenerator(get_password_len, get_uppercase_include, get_lowercase_include)
-
-print(options)
+password = generate_password(get_password_len, get_uppercase_include, get_lowercase_include)
+print(password)
