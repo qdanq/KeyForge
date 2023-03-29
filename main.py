dd97cb858bfe6ace62fd9e0e4aa24c6dd6a76894
qqbeach <danildzekelev@gmail.com>
2023-03-29 21:29:11 +0300

    Add symbols include

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import string
 
 
-def generate_password(password_len, uppercase_include=True, lowercase_include=True, numbers_include=True):
+def generate_password(password_len, uppercase_include=True, lowercase_include=True, numbers_include=True, symbols_include=True):
     characters = ''
     if uppercase_include != "n":
         characters += string.ascii_uppercase
@@ -10,6 +10,8 @@ def generate_password(password_len, uppercase_include=True, lowercase_include=Tr
         characters += string.ascii_lowercase
     if numbers_include != "n":
         characters += string.digits
+    if symbols_include != "n":
+        characters += string.punctuation
     return ''.join(random.choice(characters) for _ in range(password_len))
 
 
@@ -26,5 +28,7 @@ get_lowercase_include = input(
 get_numbers_include = input(
     "Do you want to include numbers? (y/n): ").lower() == 'y'
 
-password = generate_password(get_password_len, get_uppercase_include, get_lowercase_include)
+get_symbols_include = input("Do you want to include symbols? (y/n): ").lower() == 'y'
+
+password = generate_password(get_password_len, get_uppercase_include, get_lowercase_include, get_numbers_include, get_symbols_include)
 print(password)
